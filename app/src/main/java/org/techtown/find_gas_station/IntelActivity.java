6a28e40df9e02b8015716fbb0ec5f1eab9d3b57b
이유건 <50404123+leeugun123@users.noreply.github.com>
2022-08-08 22:12:06 +0900

    주유소 정보 가공

--- app/src/main/java/org/techtown/find_gas_station/IntelActivity.java
+++ app/src/main/java/org/techtown/find_gas_station/IntelActivity.java
@@ -3,6 +3,7 @@ package org.techtown.find_gas_station;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import org.techtown.find_gas_station.databinding.ActivityIntelBinding;
@@ -21,19 +22,39 @@ public class IntelActivity extends AppCompatActivity {
         Intent receive_intent = getIntent();
 
         String lotAddress = receive_intent.getStringExtra("lotAddress");
-        binding.lotAddress.setText(lotAddress);
+        binding.lotAddress.setText(lotAddress);//지번 주소
 
         String stAddress = receive_intent.getStringExtra("stAddress");
-        binding.stAddress.setText(stAddress);
+        binding.stAddress.setText(stAddress);//도로명 주소
 
         String tel = receive_intent.getStringExtra("tel");
-        binding.tel.setText(tel);
+        binding.tel.setText(tel);//전화번호
 
         String oil_kind = receive_intent.getStringExtra("oil_kind");
-        binding.oilKind.setText(oil_kind);
+
+        if(oil_kind.equals("N")){
+            binding.oilKind.setText("주유소");
+        }
+        else if(oil_kind.equals("Y")){
+            binding.oilKind.setText("자동차 주유소");
+        }
+        else{
+            binding.oilKind.setText("주유소/충전소 겸업");
+        }
+        //업종 구분
+
 
         String carWash = receive_intent.getStringExtra("carWash");
-        binding.carWash.setText(carWash);
+        if(carWash.equals("Y")){
+            binding.carWash.setText("존재 O");
+            binding.carWash.setTextColor(Color.parseColor("#009900"));
+        }
+        else{
+            binding.carWash.setText("존재 X");
+            binding.carWash.setTextColor(Color.parseColor("#ff0000"));
+        }
+
+
 
         String store = receive_intent.getStringExtra("store");
         binding.store.setText(store);
