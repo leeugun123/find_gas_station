75d21393e10244a1a356a50b1946b82161d18fb8
이유건 <50404123+leeugun123@users.noreply.github.com>
2023-07-29 17:35:03 +0900

    스플래시 화면에서 위치 권한 요청 구현

--- app/src/main/java/org/techtown/find_gas_station/Splash.java
+++ app/src/main/java/org/techtown/find_gas_station/Splash.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.Manifest;
@@ -16,6 +17,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 
@@ -26,8 +28,7 @@ public class Splash extends AppCompatActivity implements
 ActivityCompat.OnRequestPermissionsResultCallback{
     
     private SetViewModel setViewModel;
-    private String[] REQUIRED_PERMISSIONS  = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};  // 외부 저장소
-
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,20 +40,26 @@ ActivityCompat.OnRequestPermissionsResultCallback{
         setViewModel.insert(new Set("B027","1000","1"));
         //null 값 방지
 
-
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-
-            @Override
-            public void run() {
-
-                Intent intent = new Intent(getApplicationContext(), FragmentActivity.class);
-                startActivity(intent);
-                finish();
+        if (checkLocationPermission()) {
+            // 위치 권한이 허용된 경우, 다음 화면으로 이동
 
 
-            }
-        },500);
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+
+                @Override
+                public void run() {
+
+                    startNextActivity();
+
+                }
+            },500);
+
+
+        } else {
+            // 위치 권한 요청
+            requestLocationPermission();
+        }
 
 
     }
@@ -61,6 +68,51 @@ ActivityCompat.OnRequestPermissionsResultCallback{
     protected void onPause() {
 
         super.onPause();
+    }
+
+    // 위치 권한이 허용되었는지 확인하는 메소드
+    private boolean checkLocationPermission() {
+        return ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    // 위치 권한 요청 메소드
+    private void requestLocationPermission() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                },
+                LOCATION_PERMISSION_REQUEST_CODE);
+    }
+
+    // 위치 권한 요청 결과 처리
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // 위치 권한이 허용된 경우, 다음 화면으로 이동
+                startNextActivity();
+            } else {
+                // 위치 권한이 거부된 경우, 앱을 종료하거나 다른 처리를 진행할 수 있습니다.
+                Toast.makeText(this, "위치 권한이 거부되었습니다. 앱을 종료합니다.", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+
+    // 다음 화면으로 이동하는 메소드
+    private void startNextActivity() {
+        // 다음 화면(Activity)으로 이동하려면 해당 화면의 Intent를 생성하여 startActivity()를 호출합니다.
+        // 예시: startActivity(new Intent(this, NextActivity.class));
+
+        // 이 예시에서는 2초 후에 다음 화면으로 이동하도록 설정합니다.
+        // 적절한 다음 화면을 설정하여 수정해주세요.
+        startActivity(new Intent(this, FragmentActivity.class));
         finish();
     }
 
