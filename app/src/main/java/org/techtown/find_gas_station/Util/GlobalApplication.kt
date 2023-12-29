cfd6c95b653ae1ddd829894dc19a4edf04d7a9f9
leeugun123 <leeugun123@naver.com>
2023-12-29 18:11:50 +0900

    class -> object 변환

--- app/src/main/java/org/techtown/find_gas_station/Util/GlobalApplication.kt
+++ app/src/main/java/org/techtown/find_gas_station/Util/GlobalApplication.kt
@@ -4,15 +4,12 @@ import android.app.Application
 import com.kakao.sdk.common.KakaoSdk
 import org.techtown.find_gas_station.BuildConfig
 
-class GlobalApplication : Application() {
+object GlobalApplication : Application() {
 
-    companion object {
-        private lateinit var instance : GlobalApplication
-    }
+    private val instance by lazy { this }
 
     override fun onCreate() {
         super.onCreate()
-        instance = this
         KakaoSdk.init(this, "{" + BuildConfig.KAKAO_API_KEY + "}")
     }
 
