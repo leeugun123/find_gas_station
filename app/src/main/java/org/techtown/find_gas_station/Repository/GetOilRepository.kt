647a192120e62d6d26e16f68217315e73dcf5b36
leeugun123 <leeugun123@naver.com>
2024-01-20 15:31:42 +0900

    GetOilRepository 코루틴 적용 -> KakaoApi

--- app/src/main/java/org/techtown/find_gas_station/Repository/GetOilRepository.kt
+++ app/src/main/java/org/techtown/find_gas_station/Repository/GetOilRepository.kt
@@ -1,13 +1,10 @@
 package org.techtown.find_gas_station.Repository
 
 import android.app.Application
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.techtown.find_gas_station.Data.OilList.GasStationInfoResult
-import org.techtown.find_gas_station.Data.OilList.Result
-import org.techtown.find_gas_station.Data.OilList.StationInfo
 import org.techtown.find_gas_station.Data.TotalOilInfo
 import org.techtown.find_gas_station.Data.kakao.Request.Destination
 import org.techtown.find_gas_station.Data.kakao.Request.DirectionRequest
@@ -18,8 +15,6 @@ import org.techtown.find_gas_station.R
 import org.techtown.find_gas_station.Util.Api.ApiKey.OPI_API_KEY
 import org.techtown.find_gas_station.Util.Api.Api_Instance.kakaoRetrofitApi
 import org.techtown.find_gas_station.Util.Api.Api_Instance.opiRetrofitApi
-import org.techtown.find_gas_station.Util.Comparator.OilDistanceComparator
-import org.techtown.find_gas_station.Util.Comparator.OilPriceComparator
 import org.techtown.find_gas_station.Util.Comparator.OilRoadDistanceComparator
 import org.techtown.find_gas_station.Util.Comparator.OilSpendTimeComparator
 import org.techtown.find_gas_station.Util.Constant.ConstantGuide.JSON_FORMAT
@@ -77,6 +72,7 @@ class GetOilRepository(application : Application) {
         val inputOil = getOilType(oilKind)
 
         result?.forEach { oilInfo ->
+
             val out = GeoTrans.convert(GeoTrans.KATEC, GeoTrans.GEO, GeoTransPoint(oilInfo.gisX.toDouble(), oilInfo.gisY.toDouble()))
             getOilDetail(sort, result.size, oilInfo.id, oilInfo.osName, oilInfo.price, oilInfo.distance, inputOil,
                 getTrademarkImageResource(oilInfo.pollDivCd), out.x.toFloat(), out.y.toFloat())
@@ -93,6 +89,7 @@ class GetOilRepository(application : Application) {
         imageResource : Int, destinationX : Float, destinationY : Float) {
 
         try {
+
             val response = withContext(Dispatchers.IO) {
                 opiRetrofitApi.getOilDetail(OPI_API_KEY, JSON_FORMAT, uid)
             }
@@ -108,7 +105,7 @@ class GetOilRepository(application : Application) {
 
     }
 
-    private fun handleOilDetailResponse(gasStationDetailInfo: GasStationDetailInfoResult?, sort: String, size: Int,
+    private suspend fun handleOilDetailResponse(gasStationDetailInfo: GasStationDetailInfoResult?, sort: String, size: Int,
                                         uid: String, name: String, gasPrice: String, distance: String, inputOil: String,
                                         imageResource: Int, destinationX: Float, destinationY: Float) {
         gasStationDetailInfo?.let {
@@ -128,7 +125,7 @@ class GetOilRepository(application : Application) {
 
     }
 
-    private fun checkTempListSize(size : Int , sort : String) {
+    private suspend fun checkTempListSize(size : Int , sort : String) {
 
         if (tempList.size == size || tempList.size == 30) {
 
@@ -136,37 +133,31 @@ class GetOilRepository(application : Application) {
                 getOilKakaoApi(sort)
                 return
             }
-            if (sort == CHECK_PRICE_CONDITION) {
-                Collections.sort(tempList, OilPriceComparator())
-            } else if (sort == CHECK_TWO_DIRECT_DISTANCE) {
-                Collections.sort(tempList, OilDistanceComparator())
-            }
 
             oilListLiveData.value = tempList
-
         }
 
     }
 
 
     //카카오 api는 wgs 좌표를 사용
-    private fun getOilKakaoApi(sort: String) {
+    private suspend fun getOilKakaoApi(sort: String) {
 
         val destinations = arrayOfNulls<Destination>(tempList.size)
-
         destinationsProcessing(destinations)
 
-        kakaoRetrofitApi.getMultiDirections(DirectionRequest(Origin(HomeFragment.getWgsMyX.toDouble(), HomeFragment.getWgsMyY.toDouble()),
-                destinations, KAKAO_REQUEST_RADIUS)).enqueue(object : Callback<DirectionResponse> {
-
-                override fun onResponse(call: Call<DirectionResponse>, response: Response<DirectionResponse>) {
-                    if (response.isSuccessful)
-                        handleKakaoApiResponse(response.body() , sort)
-                }
-
-                override fun onFailure(call: Call<DirectionResponse?>, t: Throwable) {}
+        try {
+            val response = withContext(Dispatchers.IO) {
+                kakaoRetrofitApi.getMultiDirections(DirectionRequest(Origin(HomeFragment.getWgsMyX.toDouble(), HomeFragment.getWgsMyY.toDouble()),
+                    destinations, KAKAO_REQUEST_RADIUS))
             }
-        )
+
+            if (response.isSuccessful) {
+                handleKakaoApiResponse(response.body(),sort)
+            }
+
+        } catch (e: Exception) { }
+
     }
 
     private fun destinationsProcessing(destinations : Array<Destination?>) {
@@ -181,6 +172,7 @@ class GetOilRepository(application : Application) {
     }
 
     private fun handleKakaoApiResponse(directionResponse: DirectionResponse?, sort: String) {
+
         directionResponse?.let {
             val routes = it.routes
 
@@ -192,9 +184,9 @@ class GetOilRepository(application : Application) {
             }
 
             if (sort == CHECK_FOUR_SPEND_TIME) {
-               // Collections.sort(plusList, OilSpendTimeComparator())
+                Collections.sort(plusList, OilSpendTimeComparator())
             } else {
-              //  Collections.sort(plusList, OilRoadDistanceComparator())
+                Collections.sort(plusList, OilRoadDistanceComparator())
             }
 
             oilListLiveData.value = plusList
@@ -202,7 +194,6 @@ class GetOilRepository(application : Application) {
         }
 
     }
-
 
 
             private fun getTrademarkImageResource(trademark: String) =
--- app/src/main/java/org/techtown/find_gas_station/Retrofit/Kakao_RetrofitApi.kt
+++ app/src/main/java/org/techtown/find_gas_station/Retrofit/Kakao_RetrofitApi.kt
@@ -4,6 +4,7 @@ import org.techtown.find_gas_station.BuildConfig
 import org.techtown.find_gas_station.Data.kakao.Request.DirectionRequest
 import org.techtown.find_gas_station.Data.kakao.Response.DirectionResponse
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.Headers
 import retrofit2.http.POST
@@ -14,7 +15,7 @@ interface Kakao_RetrofitApi {
         "Authorization: KakaoAK " + BuildConfig.KAKAO_API_KEY
     )
     @POST("v1/destinations/directions")
-    fun getMultiDirections(@Body request : DirectionRequest): Call<DirectionResponse>
+    suspend fun getMultiDirections(@Body request : DirectionRequest) : Response<DirectionResponse>
     //다중 목적지
 
 }
