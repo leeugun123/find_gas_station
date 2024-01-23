ab1396c7c4bd84a4b4a7e679716d74e1c83add84
leeugun123 <leeugun123@naver.com>
2024-01-23 15:57:37 +0900

    scope 변경 (coroutineScope -> viewModelScope)

--- app/src/main/java/org/techtown/find_gas_station/ViewModel/SetViewModel.kt
+++ app/src/main/java/org/techtown/find_gas_station/ViewModel/SetViewModel.kt
@@ -5,15 +5,17 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import kotlinx.coroutines.coroutineScope
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import org.techtown.find_gas_station.Data.set.OilData
 import org.techtown.find_gas_station.Repository.SetRepository
 import org.techtown.find_gas_station.Data.set.RoomDB
 
-class SetViewModel(application : Application) : AndroidViewModel(application) {
+class SetViewModel(application: Application) : AndroidViewModel(application) {
 
-    val oilLocalData : LiveData<OilData>
-    private val setRepository : SetRepository
+    val oilLocalData: LiveData<OilData>
+    private val setRepository: SetRepository
 
     init {
         val oilDao = RoomDB.getAppDatabase(application).setDao()
@@ -21,17 +23,17 @@ class SetViewModel(application : Application) : AndroidViewModel(application) {
         oilLocalData = setRepository.allSets
     }
 
-    suspend fun insert(set: OilData) = coroutineScope {
+    // viewModelScope로 변경
+    suspend fun insert(set: OilData) = viewModelScope.launch(Dispatchers.IO) {
         setRepository.insert(set)
     }
 
-    suspend fun delete() = coroutineScope {
+    // viewModelScope로 변경
+    suspend fun delete() = viewModelScope.launch(Dispatchers.IO) {
         setRepository.deleteAll()
     }
 
     class Factory(val application: Application) : ViewModelProvider.Factory {
         override fun <T : ViewModel> create(modelClass: Class<T>) = SetViewModel(application) as T
     }
-
-
 }
