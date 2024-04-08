b088849a70426e6057fca65fc9272355d3110eb1
leeugun123 <leeugun123@naver.com>
2024-04-09 07:10:28 +0900

    [Fix/#1]: 클래스 이름 변경

--- app/src/main/java/org/techtown/find_gas_station/GlobalApplication.kt
+++ app/src/main/java/org/techtown/find_gas_station/GlobalApplication.kt
@@ -2,13 +2,12 @@ package org.techtown.find_gas_station
 
 import android.app.Application
 import com.kakao.sdk.common.KakaoSdk
-import org.techtown.find_gas_station.BuildConfig
 
-class GlobalApplication : Application() {
+class GasApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        KakaoSdk.init(this, "{" + BuildConfig.KAKAO_API_KEY + "}" )
+        KakaoSdk.init(this, "{" + BuildConfig.KAKAO_API_KEY + "}")
     }
 
 }
