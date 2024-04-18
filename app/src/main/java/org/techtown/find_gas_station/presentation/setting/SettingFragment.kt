dc8e2a47206fd4c2692032acc6fffeabb769304e
leeugun123 <leeugun123@naver.com>
2024-04-18 16:08:00 +0900

    [Design/#3]: 코드 최적화

--- app/src/main/java/org/techtown/find_gas_station/presentation/setting/SettingFragment.kt
+++ app/src/main/java/org/techtown/find_gas_station/presentation/setting/SettingFragment.kt
@@ -15,6 +15,21 @@ import org.techtown.find_gas_station.presentation.oilroundinfo.OilInfoViewModel
 class SettingFragment() : BaseFragment<FragmentSettingBinding>(R.layout.fragment_setting) {
 
     private val oilInfoViewModel: OilInfoViewModel by activityViewModels()
+
+    private val onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+        override fun onItemSelected(
+            adapterView: AdapterView<*>,
+            view: View,
+            position: Int,
+            id: Long
+        ) {
+            (adapterView.getChildAt(0) as? TextView)?.setTextColor(Color.BLACK)
+        }
+
+        override fun onNothingSelected(adapterView: AdapterView<*>?) {}
+    }
+
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initBinding()
@@ -25,46 +40,9 @@ class SettingFragment() : BaseFragment<FragmentSettingBinding>(R.layout.fragment
         binding.lifecycleOwner = viewLifecycleOwner
         binding.viewModel = oilInfoViewModel
         binding.goBackClick = ::goBack
-
-        binding.typeSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(
-                adapterView: AdapterView<*>,
-                view: View,
-                position: Int,
-                id: Long
-            ) {
-                (adapterView.getChildAt(0) as TextView).setTextColor(Color.BLACK)
-            }
-
-            override fun onNothingSelected(adapterView: AdapterView<*>?) {}
-        }
-
-        binding.distanceSpinner.onItemSelectedListener =
-            object : AdapterView.OnItemSelectedListener {
-                override fun onItemSelected(
-                    adapterView: AdapterView<*>,
-                    view: View,
-                    position: Int,
-                    id: Long
-                ) {
-                    (adapterView.getChildAt(0) as TextView).setTextColor(Color.BLACK)
-                }
-
-                override fun onNothingSelected(adapterView: AdapterView<*>?) {}
-            }
-
-        binding.sortSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(
-                adapterView: AdapterView<*>,
-                view: View,
-                position: Int,
-                id: Long
-            ) {
-                (adapterView.getChildAt(0) as TextView).setTextColor(Color.BLACK)
-            }
-
-            override fun onNothingSelected(adapterView: AdapterView<*>?) {}
-        }
+        binding.typeSpinner.onItemSelectedListener = onItemSelectedListener
+        binding.distanceSpinner.onItemSelectedListener = onItemSelectedListener
+        binding.sortSpinner.onItemSelectedListener = onItemSelectedListener
     }
 
 
