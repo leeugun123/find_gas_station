105e300500c44b8b0c9d1bdbf657f4515185052a
leeugun123 <leeugun123@naver.com>
2024-04-19 15:20:46 +0900

    [Design/#3]: 주유소 정보 xml 버튼 수정

--- app/src/main/java/org/techtown/find_gas_station/presentation/oilroundinfo/OilInfoViewModel.kt
+++ app/src/main/java/org/techtown/find_gas_station/presentation/oilroundinfo/OilInfoViewModel.kt
@@ -1,6 +1,5 @@
 package org.techtown.find_gas_station.presentation.oilroundinfo
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -21,11 +20,12 @@ class OilInfoViewModel : ViewModel() {
 
     var conditionChangeFlag = true
     var oilCondition = OilCondition("1000", "1", "D047")
-    var afterOilCondition = OilCondition("","","")
+    var afterOilCondition = OilCondition("", "", "")
 
 
     private val _oilListLiveData = MutableLiveData<List<TotalOilInfo>>()
-    val oilListLiveData: LiveData<List<TotalOilInfo>> get() = _oilListLiveData
+    val oilListLiveData: LiveData<List<TotalOilInfo>>
+        get() = _oilListLiveData
 
     private val stationInfoRepository = StationInfoRepository()
     //TODO("viewModel에서 객체를 생성 -> 추후 Hilt를 적용하여 객체를 주입 받아야 함.")
