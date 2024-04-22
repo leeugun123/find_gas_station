308713f7ee259ecf700a43ceb9bdf57ea31a5908
leeugun123 <leeugun123@naver.com>
2024-04-22 14:30:31 +0900

    [Ref/#3]: string 연결

--- app/src/main/java/org/techtown/find_gas_station/presentation/oilavginfo/childFragment/OilAvgViewCreated.kt
+++ app/src/main/java/org/techtown/find_gas_station/presentation/oilavginfo/childFragment/OilAvgViewCreated.kt
@@ -9,6 +9,7 @@ import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
+import org.techtown.find_gas_station.R
 import org.techtown.find_gas_station.databinding.FragmentOilAvgBinding
 import org.techtown.find_gas_station.presentation.oilavginfo.OilAvgRecyclerAdapter
 import org.techtown.find_gas_station.presentation.oilavginfo.OilAvgViewModel
@@ -34,23 +35,32 @@ class OilAvgViewCreated {
                 Entry(index.toFloat(), it.oilPrice.toFloat())
             }
 
-            val dataSet = LineDataSet(entries, "주유소 가격").apply {
-                color = Color.rgb(255, 153, 0)
-                lineWidth = 2f
-                setCircleColor(Color.rgb(253, 153, 0))
-                circleRadius = 4f
-                setDrawCircleHole(false)
-            }
+            val dataSet =
+                LineDataSet(entries, context.getString(R.string.gas_station_price)).apply {
+                    color = Color.rgb(255, 153, 0)
+                    lineWidth = 2f
+                    setCircleColor(Color.rgb(253, 153, 0))
+                    circleRadius = 4f
+                    setDrawCircleHole(false)
+                }
 
             binding.lineChart.xAxis.valueFormatter = IndexAxisValueFormatter(
-                arrayOf("7일전", "6일전", "5일전", "4일전", "3일전", "2일전", "1일전")
+                arrayOf(
+                    context.getString(R.string.seven_day_ago),
+                    context.getString(R.string.six_day_ago),
+                    context.getString(R.string.five_day_ago),
+                    context.getString(R.string.four_day_ago),
+                    context.getString(R.string.three_day_ago),
+                    context.getString(R.string.two_day_ago),
+                    context.getString(R.string.one_day_ago)
+                )
             )
 
             val lineData = LineData(dataSet)
 
             with(binding.lineChart) {
                 data = lineData
-                description.text = "최근 일주일 전국 유가 가격"
+                description.text = context.getString(R.string.recent_week_price)
                 xAxis.position = XAxis.XAxisPosition.BOTTOM
                 axisRight.isEnabled = false
                 invalidate()
