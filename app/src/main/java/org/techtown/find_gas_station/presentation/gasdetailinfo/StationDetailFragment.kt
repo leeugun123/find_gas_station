2fbe3a781982656930f4b2e3eb0fcc76fc719695
leeugun123 <leeugun123@naver.com>
2024-04-22 17:55:38 +0900

    [Feat/#5]: StationDetailFragment 생성 및 navigation 연결

---
+++ app/src/main/java/org/techtown/find_gas_station/presentation/gasdetailinfo/StationDetailFragment.kt
@@ -0,0 +1,15 @@
+package org.techtown.find_gas_station.presentation.gasdetailinfo
+
+import android.os.Bundle
+import android.view.View
+import org.techtown.find_gas_station.R
+import org.techtown.find_gas_station.databinding.FragmentStationDetailBinding
+import org.techtown.find_gas_station.presentation.BaseFragment
+
+class StationDetailFragment :
+    BaseFragment<FragmentStationDetailBinding>(R.layout.fragment_station_detail) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
