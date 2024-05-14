9408fb9e847ab40f9b12d955fbd18e945de145ff
leeugun123 <leeugun123@naver.com>
2024-05-14 21:27:02 +0900

    oilAvg Flow 적용

--- app/src/main/java/org/techtown/find_gas_station/presentation/ui/oilavginfo/OilAvgViewModel.kt
+++ app/src/main/java/org/techtown/find_gas_station/presentation/ui/oilavginfo/OilAvgViewModel.kt
@@ -3,12 +3,11 @@ package org.techtown.find_gas_station.presentation.ui.oilavginfo
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.InternalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import org.techtown.find_gas_station.data.remote.model.oilavg.OilAveragePriceInfo
 import org.techtown.find_gas_station.presentation.di.RepositoryModule
@@ -18,17 +17,13 @@ class OilAvgViewModel() : ViewModel() {
     private var getOilAvgRepository = RepositoryModule.provideGetOilAvgRepository()
 
     private val _oilAvgList = MutableStateFlow<List<OilAveragePriceInfo>>(emptyList())
-    val oilAvgList: StateFlow<List<OilAveragePriceInfo>> get() = _oilAvgList.stateIn(
-        initialValue = emptyList(),
-        started = SharingStarted.WhileSubscribed(5_000),
-        scope = viewModelScope
-    )
+    val oilAvgList: StateFlow<List<OilAveragePriceInfo>> get() = _oilAvgList
 
     fun fetchOilAvg(prodCd: String) {
         viewModelScope.launch {
             getOilAvgRepository.getOilAvg(prodCd)
                 .catch { e ->
-                    Log.e("TAG", e.message.toString())
+                    Log.e("TAG",e.message.toString())
                 }
                 .collect {
                     _oilAvgList.value = it
