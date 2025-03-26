0ffbcc677dd309fd4c6ad06f29f691183722e08a
leeugun123 <leeugun123@naver.com>
2025-03-26 12:32:12 +0900

    fix : 카카오 SDK 초기화 하도록 변경

--- app/src/main/java/org/techtown/find_gas_station/GasApplication.kt
+++ app/src/main/java/org/techtown/find_gas_station/GasApplication.kt
@@ -1,6 +1,7 @@
 package org.techtown.find_gas_station
 
 import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
 import org.techtown.find_gas_station.di.RepositoryModule
 
@@ -9,7 +10,7 @@ import org.techtown.find_gas_station.di.RepositoryModule
 class GasApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        // KakaoSdk.init(this, "{" + BuildConfig.KAKAO_API_KEY + "}")
+        KakaoSdk.init(this, "{" + BuildConfig.KAKAO_API_KEY + "}")
         RepositoryModule.init(this)
     }
 }
